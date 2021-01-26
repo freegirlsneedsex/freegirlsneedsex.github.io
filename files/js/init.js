604702af80b500f9450381ffca77f88841a78221
freegirlsneedsex <78021017+freegirlsneedsex@users.noreply.github.com>
2021-01-26 13:32:23 +0300

    Add files via upload

---
+++ files/js/init.js
@@ -0,0 +1,10 @@
+var config = {
+    apiKey: "AIzaSyD6pG3mt2yJ5nGEDc4PKoCrR9KMp-tVEpM",
+    authDomain: "cpalab-11b52.firebaseapp.com",
+    databaseURL: "https://cpalab-11b52.firebaseio.com",
+    projectId: "cpalab-11b52",
+    storageBucket: "cpalab-11b52.appspot.com",
+    messagingSenderId: "355500306335",
+    appId: "1:355500306335:web:dd1fba9f08a63dc48517aa"
+};
+firebase.initializeApp(config);
